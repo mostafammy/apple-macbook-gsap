8e54a56f6a3d36773b18ea5b5c4faa1a95dc9854
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-10-19 19:38:34 +0300

    add Hero component with video playback and promotional content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import type {JSX} from "react";
 import NavBar from "./components/NavBar.tsx";
+import Hero from "./components/Hero.tsx";
 
 export const App = (): JSX.Element => {
     return (
         <main>
             <NavBar/>
+            <Hero/>
         </main>
     );
 }
