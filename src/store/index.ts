35f030e61ba6f505d91f1e528a1220231c6424a0
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-10-20 20:27:06 +0300

    add Features component for interactive feature display and 3D model animations

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,8 @@ interface MacbookStoreType {
     setColor: (color: string) => void;
     scale: number;
     setScale: (scale: number) => void;
+    texture: string;
+    setTexture: (texture: string) => void;
     reset: () => void;
 }
 
@@ -15,6 +17,9 @@ const useMacBookStore: UseBoundStore<StoreApi<MacbookStoreType>> = create((set) 
     scale: 0.08,
     setScale: (scale: number) => set({scale}),
 
-    reset: () => set({color: '#2e2c2e', scale: 0.08})
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture: string) => set({texture}),
+
+    reset: () => set({color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4'})
 }));
 export default useMacBookStore;
